3be1045bd7a65a6d618cff11bef57ebfe95c97eb
Mathias <mathias.skallerud@gmail.com>
2021-03-12 11:19:29 +0100

    Add the data files

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,11 +11,11 @@ import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
 
-    val CALL_007_VEHICLE_03 = parseInstance("/Users/mathias/Desktop/TransportingGoods/src/main/kotlin/data/Call_7_Vehicle_3.txt")
-    val CALL_018_VEHICLE_05 = parseInstance("/Users/mathias/Desktop/TransportingGoods/src/main/kotlin/data/Call_18_Vehicle_5.txt")
-    val CALL_035_VEHICLE_07 = parseInstance("/Users/mathias/Desktop/TransportingGoods/src/main/kotlin/data/Call_035_Vehicle_07.txt")
-    val CALL_080_VEHICLE_20 = parseInstance("/Users/mathias/Desktop/TransportingGoods/src/main/kotlin/data/Call_080_Vehicle_20.txt")
-    val CALL_130_VEHICLE_40 = parseInstance("/Users/mathias/Desktop/TransportingGoods/src/main/kotlin/data/Call_130_Vehicle_40.txt")
+    val CALL_007_VEHICLE_03 = parseInstance("src/main/kotlin/data/Call_7_Vehicle_3.txt")
+    val CALL_018_VEHICLE_05 = parseInstance("src/main/kotlin/data/Call_18_Vehicle_5.txt")
+    val CALL_035_VEHICLE_07 = parseInstance("src/main/kotlin/data/Call_035_Vehicle_07.txt")
+    val CALL_080_VEHICLE_20 = parseInstance("src/main/kotlin/data/Call_080_Vehicle_20.txt")
+    val CALL_130_VEHICLE_40 = parseInstance("src/main/kotlin/data/Call_130_Vehicle_40.txt")
     runAlgo(CALL_080_VEHICLE_20, SimulatedAnnealing())
 
 
